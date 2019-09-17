7e577588ce6bd5d24c9493ed94986926040588f5
pinkienort <cantabilehisa@gmail>
2019-09-17 22:45:01 +0900

    rm unsed files

--- src/Props.ts
+++
@@ -1,8 +0,0 @@
-import Size from './Size';
-
-export default new (class Props {
-  public readonly size: Size = { x: 64, y: 64 };
-  public readonly interval: number = 10;
-
-  constructor() {}
-})();
